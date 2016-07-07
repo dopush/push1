0c1cf102213389428cc02eab9a7f1de853896855
pushpendra <push_cs@yahoo.com>
2016-07-07 06:03:27 +0000

    hello wrold in java

---
+++ src/main/java/IqHello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by iqnect on 7/7/16.
+ */
+package com.iqnect;
+
+public class IqHello {
+    public static void main(String[] args){
+        System.out.println("hello iqnect!");
+    }
+}
